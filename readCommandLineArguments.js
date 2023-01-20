9d860a6df04b57aea69acf3d5eee03637749ec7c
Pankajkumar057 <122426939+Pankajkumar057@users.noreply.github.com>
2023-01-20 22:49:23 +0530

    Add files via upload

---
+++ readCommandLineArguments.js
@@ -0,0 +1,18 @@
+const name=process.argv[2];
+
+console.log(`Hello ${name}`);
+
+// console.log(process)
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+//   readline.question("",name => {
+//     console.log(`Hello ${name}!`);
+//     readline.close();
+// });
+
+
+// const prompt = require('prompt-sync')();
+// const name = prompt('');
+// console.log(`hello ${name}`);
